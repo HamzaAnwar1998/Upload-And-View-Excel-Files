59570216ad459895932c70001278d4257e648ebd
Hamza <anwarhamza919@gmail.com>
2023-05-08 19:31:46 +0500

    write tutorial code

--- src/App.js
+++ src/App.js
@@ -5,35 +5,34 @@ function App() {
 
   // onchange states
   const [excelFile, setExcelFile] = useState(null);
-  const [error, setError] = useState(null);
+  const [typeError, setTypeError] = useState(null);
 
   // submit state
   const [excelData, setExcelData] = useState(null);
 
-  // onchange
-  const handleFileInput=(e)=>{
-    const fileType=['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','text/csv','application/vnd.ms-excel'];
-    let selecteFile = e.target.files[0];
-    if(selecteFile){
-      console.log(selecteFile.type);
-      if(selecteFile&&fileType.includes(selecteFile.type)){
+  // onchange event
+  const handleFile=(e)=>{
+    let fileTypes = ['application/vnd.ms-excel','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','text/csv'];
+    let selectedFile = e.target.files[0];
+    if(selectedFile){
+      if(selectedFile&&fileTypes.includes(selectedFile.type)){
+        setTypeError(null);
         let reader = new FileReader();
-        reader.readAsArrayBuffer(selecteFile);
+        reader.readAsArrayBuffer(selectedFile);
         reader.onload=(e)=>{
-          setError(null);
           setExcelFile(e.target.result);
         }
       }
       else{
-        setError('Please select only excel files');
+        setTypeError('Please select only excel file types');
         setExcelFile(null);
       }
     }
     else{
-      console.log('please select your file');
+      console.log('Please select your file');
     }
   }
-
+  
   // submit event
   const handleFileSubmit=(e)=>{
     e.preventDefault();
@@ -49,13 +48,14 @@ function App() {
   return (
     <div className="wrapper">
 
+      <h3>Upload & View Excel Sheets</h3>
+
       {/* form */}
       <form className="form-group custom-form" onSubmit={handleFileSubmit}>
-        <input type="file" className="form-control" required
-        onChange={handleFileInput}/>
+        <input type="file" className="form-control" required onChange={handleFile} />
         <button type="submit" className="btn btn-success btn-md">UPLOAD</button>
-        {error&&(
-          <div className="alert alert-danger" role="alert">{error}</div>
+        {typeError&&(
+          <div className="alert alert-danger" role="alert">{typeError}</div>
         )}
       </form>
 
@@ -64,26 +64,29 @@ function App() {
         {excelData?(
           <div className="table-responsive">
             <table className="table">
-            <thead>
-              <tr>
-                {Object.keys(excelData[0]).map((key) => (
-                  <th key={key}>{key}</th>
-                ))}
-              </tr>
-            </thead>
-              <tbody>
-              {excelData.map((individualExcelData, index) => (
-                <tr key={index}>
-                  {Object.keys(individualExcelData).map((key) => (
-                    <td key={key}>{individualExcelData[key]}</td>
+
+              <thead>
+                <tr>
+                  {Object.keys(excelData[0]).map((key)=>(
+                    <th key={key}>{key}</th>
                   ))}
                 </tr>
-              ))}
+              </thead>
+
+              <tbody>
+                {excelData.map((individualExcelData, index)=>(
+                  <tr key={index}>
+                    {Object.keys(individualExcelData).map((key)=>(
+                      <td key={key}>{individualExcelData[key]}</td>
+                    ))}
+                  </tr>
+                ))}
               </tbody>
+
             </table>
           </div>
         ):(
-          <div>No Data is uploaded yet</div>
+          <div>No File is uploaded yet!</div>
         )}
       </div>
 
